289a80b67d87f7ddbdba78e83ca4593404d63a64
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-02-06 11:44:06 +0500

    06.02.2025 improve rag

--- src/rag/helpers.py
+++ src/rag/helpers.py
@@ -4,5 +4,10 @@ if TYPE_CHECKING:
     from langchain_core.documents import Document
     
     
-def parse_documents_content(documents: List["Document"]) -> str:
+def parse_documents_content(
+    documents: List["Document"],
+    k: int = 5
+) -> str:
+    if len(documents) > k:
+        documents = documents[:k]
     return "\n\n".join([document.page_content for document in documents])
